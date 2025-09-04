30cdc5698cc80ed145e16b5cb705548a3cd122a9
Anis  Ouersighni <125597087+AnisOuersighni@users.noreply.github.com>
2025-09-05 01:22:37 +0200

    Add files via upload

---
+++ Identity_Breach/setup/database/schema.sql
@@ -0,0 +1,9 @@
+SET GLOBAL event_scheduler = ON;
+USE intelDB;
+
+CREATE TABLE soldiers (
+username char(20),
+password char(20)
+);
+
+INSERT INTO soldiers VALUES('admin','S2VlcHlvdXJwYXNzd29yZFNhZmVDYXB0YWluMTIK');
